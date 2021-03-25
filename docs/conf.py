6448d73112524b70f0c0698c045d4afb0afe9c21
Clark Teeple <cbteeple@github.com>
2021-03-25 08:47:35 -0400

    added logo and several visual tweaks

--- docs/conf.py
+++ docs/conf.py
@@ -68,6 +68,14 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'sphinx_rtd_theme'
 
+html_logo = "img/logo.png"
+html_theme_options = {
+    'logo_only': True,
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'style_nav_header_background': '#f0f0f0',
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
